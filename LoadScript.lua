be56696475ace06b664a468a3e9370bc46c8a970
Bricky <janjira.petkum@gmail.com>
2026-05-19 20:22:01 +0700

    Update PC script URL in LoadScript.lua

--- LoadScript.lua
+++ LoadScript.lua
@@ -5,5 +5,5 @@ print("Loading...")
 if is_mobile then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/windbreaker7/MEGGD-Script-Scanner/refs/heads/main/Device/MEGGD_Script_Scanner(Mobile).lua", true))()
 else
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/SUUUUUS00000/MEGGD-Script-Scanner/refs/heads/main/Device/MEGGD%20Script%20Scanner(PC).lua", true))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/windbreaker7/MEGGD-Script-Scanner/refs/heads/main/Device/MEGGD_Script_Scanner(PC).lua", true))()
 end
