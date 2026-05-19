eea8d8a55146dd611082733b23ba8f06c21abd62
Bricky <janjira.petkum@gmail.com>
2026-05-19 17:26:03 +0700

    Update mobile script URL in LoadScript.lua

--- LoadScript.lua
+++ LoadScript.lua
@@ -3,7 +3,7 @@ local uis = game:GetService("UserInputService")
 local is_mobile = uis.TouchEnabled and not uis.KeyboardEnabled
 print("Loading...")
 if is_mobile then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/SUUUUUS00000/MEGGD-Script-Scanner/refs/heads/main/Device/MEGGD%20Script%20Scanner(Mobile).lua", true))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/windbreaker7/MEGGD-Script-Scanner/refs/heads/main/Device/MEGGD_Script_Scanner(Mobile).lua", true))()
 else
     loadstring(game:HttpGet("https://raw.githubusercontent.com/SUUUUUS00000/MEGGD-Script-Scanner/refs/heads/main/Device/MEGGD%20Script%20Scanner(PC).lua", true))()
 end
